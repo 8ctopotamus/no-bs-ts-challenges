c84a2ab3bc24a93e4e798da335764cffa9d31cc5
Josh <zylo.codes@gmail.com>
2023-06-29 08:24:49 -0500

    01 - basics, types, fns, fn overloading

--- 01/index.ts
+++ 01/index.ts
@@ -1,4 +1,4 @@
-import houses from './houses'
+import houses from './houses.json'
 
 interface House {
   name: string
@@ -9,33 +9,23 @@ interface HouseWithID extends House {
   id: number
 }
   
-  function findHouses(houses: string): HouseWithID[];
-  function findHouses(
-    houses: string,
-    filter: (house: House) => boolean
-  ): HouseWithID[];
-  function findHouses(houses: House[]): HouseWithID[];
-  function findHouses(
-    houses: House[],
-    filter?: (house: House) => boolean
-  ): HouseWithID[];
-  function findHouses(
-    input: string | House[],
-    filter?: (house: House) => boolean
-  ): HouseWithID[] {
-    const houses: House[] = typeof input === 'string' 
-      ? JSON.parse(input)
-      : input 
+function findHouses(
+  input: string | House[],
+  filter?: (house: House) => boolean
+): HouseWithID[] {
+  const houses: House[] = typeof input === 'string' 
+    ? JSON.parse(input)
+    : input 
 
-    return (filter ? houses.filter(filter) : houses)
-      .map(house => ({
-        id: houses.indexOf(house),
-        ...house
-      }));
-  };
-  
-  console.log(
-    findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
-  );
-  
-  console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
+  return (filter ? houses.filter(filter) : houses)
+    .map(house => ({
+      ...house,
+      id: houses.indexOf(house),
+    }));
+};
+
+console.log(
+  findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
+);
+
+console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # no-bs-ts-challenges
-Doing the "No BS TS" challenges
+Doing the ["No BS TS"](https://www.youtube.com/watch?v=GsRnExrC89A&list=PLNqp92_EXZBJYFrpEzdO2EapvU0GOJ09n&index=5) challenges.
+
+To run: `npx ts-node index.ts`
