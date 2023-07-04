f227b581e02130d9ff648ab57456929557e8ca5e
Josh <zylo.codes@gmail.com>
2023-07-04 12:30:56 -0500

    classes

--- 03/classes.ts
+++ 03/classes.ts
@@ -3,8 +3,13 @@ interface DB {
   set(id: string, value: string): void
 }
 
+interface Persistable {
+  saveToString(): string
+  restoreFromString(storedState: string): void
+}
+
 class InMemoryDB implements DB {
-  private db: Record<string, string> = {}
+  protected  db: Record<string, string> = {}
   get(id: string): string {
     return this.db[id]
   }
@@ -13,7 +18,22 @@ class InMemoryDB implements DB {
   }
 }
 
-const myDB = new InMemoryDB()
+class PersistentMemoryDB extends InMemoryDB implements Persistable {
+  saveToString(): string { 
+    return JSON.stringify(this.db)
+  }
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState)
+  }
+}
+
+const myDB = new PersistentMemoryDB()
 myDB.set('foo', 'bar')
-// myDB.db['foo'] = 'bazz' // can't directly update bc it's private
+// myDB.db['foo'] = 'bazz' // can't directly update bc it's PROTECTED
 console.log (myDB.get('foo') )
+const output = myDB.saveToString()
+console.log ( output )
+
+const myDB2 = new PersistentMemoryDB()
+myDB2.restoreFromString(output)
+console.log( myDB2.saveToString() )
