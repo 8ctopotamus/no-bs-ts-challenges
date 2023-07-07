09e6fc6b9654bfcaf286b56b80f04026419b14cc
Josh <zylo.codes@gmail.com>
2023-07-07 18:17:48 -0500

    Classes + Generics

--- 03/classes-generics.ts
+++ 03/classes-generics.ts
@@ -1,4 +1,4 @@
-interface DB<K, T> {
+interface Database<T, K> {
   get(id: K): T
   set(id: K, value: T): void
 }
@@ -10,7 +10,7 @@ interface Persistable {
 
 type DBKeyType = string | number | symbol
 
-class InMemoryDB<K extends DBKeyType, T> implements DB<K, T> {
+class InMemoryDataBase<T, K extends DBKeyType> implements Database<T, K extends DBKeyType> {
   protected  db: Record<K, T> = {} as Record<K, T>
   get(id: K): T {
     return this.db[id]
@@ -20,7 +20,7 @@ class InMemoryDB<K extends DBKeyType, T> implements DB<K, T> {
   }
 }
 
-class PersistentMemoryDB<K extends DBKeyType, T> extends InMemoryDB<K extends DBKeyType, T> implements Persistable {
+class PersistentMemoryDataBase<T, K extends DBKeyType> extends InMemoryDataBase<T, K extends DBKeyType> implements Persistable {
   saveToString(): string { 
     return JSON.stringify(this.db)
   }
@@ -29,7 +29,7 @@ class PersistentMemoryDB<K extends DBKeyType, T> extends InMemoryDB<K extends DB
   }
 }
 
-const myDB = new PersistentMemoryDB<T, K extends DBKeyType>()
+const myDB = new PersistentMemoryDataBase<number, string>()
 myDB.set('foo', 22)
 // myDB.db['foo'] = 'bazz' // can't directly update bc it's PROTECTED
 console.log (myDB.get('foo') )
@@ -39,6 +39,6 @@ console.log (myDB.get('foo') )
 const output = myDB.saveToString()
 console.log ( output )
 
-const myDB2 = new PersistentMemoryDB<number>()
+const myDB2 = new PersistentMemoryDataBase<number, string>()
 myDB2.restoreFromString(output)
 console.log( myDB2.saveToString() )
