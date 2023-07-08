7321c8393ca921c80befe2939920153668abacac
Josh <zylo.codes@gmail.com>
2023-07-08 17:43:45 -0500

    save

---
+++ 03/mapped-types.ts
@@ -0,0 +1,41 @@
+type MyFlexibleDogInfo = {
+  name: string
+  [key: string]: string | number
+}
+
+const dog: MyFlexibleDogInfo = {
+  name: 'LG',
+  breed: 'Mutt',
+  age: 22,
+}
+
+interface DogInfo {
+  name: string
+  age: number
+}
+
+type OptionsFlag<Type> = {
+  [Property in keyof Type]: boolean
+}
+
+type DogInfoOptions = OptionsFlag<DogInfo>
+
+type Listeners<Type> = {
+  [Property in keyof Type as `on${Capitalize<string & Property>}Change`]: (newValue: Type[Property]) => void 
+}
+
+function listenToObject<T>(obj: T, listeners: Listeners<T>):void {
+  throw 'Needs to be implemented'
+}
+
+const dog2: DogInfo = {
+  name: 'LG',
+  age: 13,
+}
+
+type DogIinfoListeners = Listeners<DogInfo>
+
+listenToObject(dog2, {
+  onNameChange: (v: string) => {},
+  onAgeChange: (v: number) => {},
+})
