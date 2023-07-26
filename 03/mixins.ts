b32156a9cda3fb9145ec656173b1dfe76a383b31
Josh <zylo.codes@gmail.com>
2023-07-26 15:04:44 -0500

    save

---
+++ 03/mixins.ts
@@ -0,0 +1,55 @@
+// functions creating functions
+function myLogFunction() {
+  return (str: string) => {
+    console.log(str)
+  }
+}
+
+// functions creating classes
+function createLoggerClass() {
+  return class MyLoggerClass {
+      private completeLog: string = ''
+      log(str: string) {
+        console.log(str)
+        this.completeLog += str + '\n'
+      }
+      dumpLog() {
+        return this.completeLog
+      }
+  }
+}
+
+const MyLogger = createLoggerClass()
+const logger2 = new MyLogger()
+logger2.log('Foo')
+console.log(logger2.dumpLog())
+
+// functions creating generic classes
+function CreateSimpleMemoryDatabase<T>() {
+  return class SimpleMemoryDatabase {
+    private db: Record<string, T> = {}
+
+    set(id: string, value: T) {
+      this.db[id] = value
+    }
+    get(id: string) {
+      return this.db[id]
+    }
+    getObject(): object {
+      return this.db
+    }
+  }
+}
+
+const StringDatabase = CreateSimpleMemoryDatabase<string>()
+
+const sdb1 = new StringDatabase()
+sdb1.set('a', 'hello')
+
+function Dumpable(Base) {
+  return class Dumpable extends Base {
+    dump() {
+      console.log(this.getObject())
+    }
+  }
+}
